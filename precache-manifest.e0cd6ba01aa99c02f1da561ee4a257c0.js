31fb76108ad94679e451778bfc42f1aaf0d71c43
Axel <axosar@gmail.com>
2019-12-02 10:26:40 -0600

    Updates

---
+++ precache-manifest.e0cd6ba01aa99c02f1da561ee4a257c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd0c61c54235cce38402b9d9fdcaf07",
+    "url": "/navidad/index.html"
+  },
+  {
+    "revision": "ddc24ca684f8e2e1fabc",
+    "url": "/navidad/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2504323cc98e79c52f9d",
+    "url": "/navidad/static/js/2.ec24fd31.chunk.js"
+  },
+  {
+    "revision": "ddc24ca684f8e2e1fabc",
+    "url": "/navidad/static/js/main.b0a0fa83.chunk.js"
+  },
+  {
+    "revision": "ec5176586c86fd7b5d12",
+    "url": "/navidad/static/js/runtime-main.a19c6546.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/navidad/static/media/logo.25bf045c.svg"
+  }
+]);
